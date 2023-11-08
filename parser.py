f366cdeb22874eeea55d466800d035248cc6e4cf
Zaremas <67574377+Zaremas@users.noreply.github.com>
2023-11-08 12:40:26 +0600

    Update parser.py

--- parser.py
+++ parser.py
@@ -3,6 +3,13 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
+columns = [
+        "Title", "Price", "Location", "Rooms", "Bail", "TypeOfBuilding", "ResidentialComplex",
+        "YearBuilt", "Floor", "Area", "PreviouslyDorm", "Conditions", "Phone",
+        "Internet", "Bathroom", "Balcony", "BalconyIsGlazed", "Door", "Parking", "Furnished",
+        "Flooring", "CeilingHeight", "Security", "Miscellaneous","ExchangePossible", "Description"
+    ]
+
 index = {
     "Title": 0,
     "Price": 1,
@@ -28,7 +35,8 @@ index = {
     "Потолки": 21,
     "Безопасность": 22,
     "Разное": 23,
-    "Возможен обмен": 24
+    "Возможен обмен": 24,
+    "Oписание": 25
 }
 
 def get_links(soup):
@@ -39,29 +47,46 @@ def get_links(soup):
     return links
 
 def parse_page(url):
-    time.sleep(1)
     page = requests.get(url)
     html = page.content
-    data = [None]*25
+    data = [None]*26
     soup = BeautifulSoup(html, 'html.parser')
+
+    title = soup.find('div', class_='offer__advert-title').h1.text.strip()
+    data[index["Title"]] = title
+
+    price = soup.find(['div','p'], class_='offer__price').contents[0].text.strip()
+    data[index["Price"]] = price
+
+    parameters1 = soup.find_all("div", {"class":"offer__info-item"})
+    for parameter in parameters1:
+        attr = parameter.find("div", {"class":"offer__info-title"}).text.strip()
+        if attr in index:
+            data[index[attr]] = parameter.find('div',{"class":"offer__advert-short-info"}).contents[0].text
+
     if soup.find('div',{'class':"offer__parameters-mortgaged"}):
         data[index["В залоге"]] = True
     else:
         data[index["В залоге"]] = False
 
-    parameters = soup.find("div", {"class":"offer__parameters"}).find_all("dl")
-    for parameter in parameters:
+    parameters2 = soup.find("div", {"class":"offer__parameters"}).find_all("dl")
+    for parameter in parameters2:
         attr = parameter.find('dt').text.strip()
         if attr in index:
             data[index[attr]] = parameter.find('dd').text
-    print('finished a row')
+
+    if soup.find('div',{'class':"a-text a-text-white-spaces"}):
+        data[index["Oписание"]] = soup.find('div',{'class':"a-text a-text-white-spaces"}).text
+
+    #print('finished a row')
     return data
 
 
 
 url = 'https://krisha.kz/prodazha/kvartiry/almaty/?page=1'
+rows = []
 
-limit = 5
+#limit = 10
 page_counter=1
 
 while True:
@@ -70,30 +95,24 @@ while True:
     soup = BeautifulSoup(response.text, 'html.parser')
     links = get_links(soup)
 
-    rows = []
     for link in links:
         row = parse_page(link)
         rows.append(row)
+        time.sleep(1)
 
-    columns = [
-        "Title", "Price", "Location", "Rooms", "Bail", "TypeOfBuilding", "ResidentialComplex",
-        "YearBuilt", "Floor", "Area", "PreviouslyDorm", "Conditions", "Phone",
-        "Internet", "Bathroom", "Balcony", "BalconyIsGlazed", "Door", "Parking",
-        "Furnished", "Flooring", "CeilingHeight", "Security", "Miscellaneous","ExchangePossible"
-    ]
-    df = pd.DataFrame(rows, columns=columns)
-    
     next_page_url = soup.find("a", {"class": "paginator__btn--next"})
 
+    print("page done:", page_counter)
+
     if next_page_url:
-        URL = "https://krisha.kz"+next_page_url["href"]
+        url = "https://krisha.kz"+next_page_url["href"]
     else:
         break
-    
-    if page_counter == limit:
-        break
-    print("page done:", page_counter)
-    page_counter+=1
-    time.sleep(1)
 
+    #if page_counter == limit:
+    #    break
+
+    page_counter+=1
+
+df = pd.DataFrame(rows, columns=columns)
 df.to_csv("krisha.csv")
